448dc8ea273c341aee1a1af7e00ab1d67bd4030b
Greenbird122 <ijakaapaul2006@gmail.com>
2026-04-19 00:34:13 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ success: false, message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => 
     console.log(`🚀 Tulia Backend running on port ${PORT}`)
 );
